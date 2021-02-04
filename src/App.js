b417af558584c18e8db61049b78a552df2182975
Simon.Brandone <simon.brandone@utb.ecutbildning.se>
2021-02-04 12:00:41 +0100

    v2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, { Component } from 'react';
-import Goals from './components/goals';
 
-class App extends Component {
+import React, { useEffect, useState } from 'react';
 
-  state = {
-    goals: []
-  }
+const fetchURL = "https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=true";
+const getItems = () => fetch(fetchURL).then(res => res.json());
 
-  componentDidMount() {
-    fetch('https://unstats.un.org/SDGAPI/v1/sdg/Goal/List?includechildren=true')
-    .then(res => res.json())
-    .then((data) => {
-      this.setState({ goals: data })
-    })
-    .catch(console.log)
-  }
 
-  render() {
-    return (
-      <Goals goals={this.state.goals} />
-    )
-  }
+function App() {
+  const [items, setItems] = useState([]);
+  const [targets, setTargets] = useState([]);
+  const [descs, setDescs] = useState('');
+
+  useEffect(() => {
+    getItems().then(data => setItems(data));
+  }, []);
+
+  const handleGoalDesc = e => {
+    setDescs(e.description)
+    setTargets(e.targets)
+
+  };
+
+  return (
+    <div className="container">
+      <div className="title-container">
+      <h1 className="title-text">FN Goals</h1>
+      </div>
+      <div className="card-container">
+      {items.map((item, i) => (
+    <div className="card" onClick={() => handleGoalDesc(item)} key={i}>
+    <ul className="card-body">
+      <h5 className="card-title" key={i}>Goal {i+1}:  {item.title}</h5>
+    </ul>
+  </div>
+        
+      ))}
+    </div>
+          <div className="card-info">
+          <h4 key={"Descs"}>{descs}</h4>
+            { targets.map((target, i) => {
+              return <p className="card-text" key={i}>{target.description}</p>
+            })}
+        </div>
+      </div>
+  );
 }
 
 export default App;
